3d197370cc7ae4c033c492d039eaa6527d12b2a1
Alexander Kaluzhny <alexander.kaluzhny@gmail.com>
2020-05-11 22:03:11 +0300

    front-end files moved to `front-end` folder

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <br />
         <a
           className="App-link"
           href="https://reactjs.org"
